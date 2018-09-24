03c2752e3bd7ff81b8e39311edab3d379528715a
pflaz <pf@szach.net>
2018-09-24 14:44:34 +0200

    Mp3 Player - controls on the main pane

--- Mp3Player/src/pl/javastart/mp3player/main/Main.java
+++ Mp3Player/src/pl/javastart/mp3player/main/Main.java
@@ -11,10 +11,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        final String appName = "Mp3Player v0.2";
+        final String appName = "Mp3Player v0.3";
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 400, 400);
-        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         primaryStage.setTitle(appName);
         primaryStage.setScene(scene);
         primaryStage.show();
