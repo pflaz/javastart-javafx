73d36dab93e0dd6b279caab7d178a29f51d7bd2b
pflaz <pf@szach.net>
2018-09-24 15:52:57 +0200

    MP3 player - controller for Main pane

---
+++ Mp3Player/src/pl/javastart/mp3player/controller/MainController.java
@@ -0,0 +1,54 @@
+package pl.javastart.mp3player.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private Menu fileMenu;
+
+    @FXML
+    private MenuItem fileMenuItem;
+
+    @FXML
+    private MenuItem dirMenuItem;
+
+    @FXML
+    private MenuItem closeMenuItem;
+
+    @FXML
+    private MenuItem aboutMenuItem;
+
+    @FXML
+    private TableView<?> contentTable;
+
+    @FXML
+    private Button prevButton;
+
+    @FXML
+    private ToggleButton playButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private Slider songSlider;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- Mp3Player/src/pl/javastart/mp3player/main/Main.java
+++ Mp3Player/src/pl/javastart/mp3player/main/Main.java
@@ -4,17 +4,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        final String appName = "Mp3Player v0.3";
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 400, 400);
-//        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        final String appName = "Mp3Player v0.4";
+        Parent parent = FXMLLoader.load(getClass().getResource(
+                "/pl/javastart/mp3player/view/MainPane.fxml"
+        ));
+        Scene scene = new Scene(parent);
         primaryStage.setTitle(appName);
         primaryStage.setScene(scene);
         primaryStage.show();
